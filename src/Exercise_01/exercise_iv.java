6d99af859b323640ed95a1f5d154dd881bcc82bd
NiluxyKrish <niluxyjana@gmail.com>
2023-01-11 14:46:02 +0530

    changes

--- src/Exercise_01/exercise_iv.java
+++ src/Exercise_01/exercise_iv.java
@@ -10,9 +10,9 @@ public class exercise_iv {
  System.out.println(num[i]);
  }
  System.out.println();
- for (int i = num.length-1; i >= 0; i--) {  
+ for (int i = num.length-1; i >= 0; i--) {
 	 System.out.println(num[i]);	
- 
+	 System.out.println("");
  }
 
 }
